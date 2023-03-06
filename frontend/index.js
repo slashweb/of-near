4ecb52f2482fb1b83d282c956a054764d97484e7
Victor Lopez <vlopez@Victors-MacBook-Pro.local>
2023-03-06 17:12:57 -0600

    Haciendo prueba de instalación con Chakra

--- frontend/index.js
+++ frontend/index.js
@@ -1,23 +1,27 @@
 // React
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import App from './App';
-
+import Home from './pages/Home'
 // NEAR
-import { Wallet } from './near-wallet';
+import {Wallet} from './near-wallet';
+import {ChakraProvider} from '@chakra-ui/react'
 
 const CONTRACT_ADDRESS = process.env.CONTRACT_NAME
 
 // When creating the wallet you can optionally ask to create an access key
 // Having the key enables to call non-payable methods without interrupting the user to sign
-const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS })
+const wallet = new Wallet({createAccessKeyFor: CONTRACT_ADDRESS})
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 // Setup on page load
 window.onload = async () => {
-  const isSignedIn = await wallet.startUp()
+    const isSignedIn = await wallet.startUp()
 
-  root.render(
-    <App isSignedIn={isSignedIn} contractId={CONTRACT_ADDRESS} wallet={wallet} />
-  );
+    root.render(
+        // <App isSignedIn={isSignedIn} contractId={CONTRACT_ADDRESS} wallet={wallet} />
+        <ChakraProvider>
+            <Home/>
+        </ChakraProvider>
+    );
 }
